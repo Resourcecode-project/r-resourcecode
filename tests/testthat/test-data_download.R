9cf59f940c379f5385b2cd8d5a045cae5d00904e
RAILLARD <nicolas.raillard@ifremer.fr>
2023-09-12 10:55:57 +0200

    test all cases for `get_parameters`

--- tests/testthat/test-data_download.R
+++ tests/testthat/test-data_download.R
@@ -1,8 +1,9 @@
 test_that("downloading parameters data works",{
   skip_if_offline()
-  dat = get_parameters(parameters = c('hs','tp'),node=42,start="1994-01-01Z00:00:00",end = 760057200)
+  dat = get_parameters(parameters = c('hs','tp'),node=42,start="1994-01-01 00:00:00 UTC",end = 760057200)
   expect_s3_class(dat,"data.frame")
   expect_equal(names(dat),c('time','hs','tp'))
   expect_equal(NROW(dat),24*31)
+  expect_equal(get_parameters(parameters = c('hs','tp'),node=42,start=757378800,end = "1994-01-31 23:00:00 UTC"),dat)
 })
 
