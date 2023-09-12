b4515050a38c8a5e5ae9e8bb72cb5d925fd1f7e0
RAILLARD <nicolas.raillard@ifremer.fr>
2023-09-12 22:18:07 +0200

    Update test for spectral data

--- tests/testthat/test-data_download.R
+++ tests/testthat/test-data_download.R
@@ -9,21 +9,21 @@ test_that("downloading parameters data works",{
 
 test_that("downloading 1D spectral data works",{
   skip_if_offline()
-  spec = get_1Dspectrum(1L,start = "1994-01-01 00:00:00 UTC",end="1994-01-01 00:00:00 UTC")
-  expect_equal(spec, get_1Dspectrum(1L,start =757382400,end=757382400))
+  spec = get_1Dspectrum(1L,start = "1994-12-01 00:00:00 UTC",end="1995-01-31 00:00:00 UTC")
+  expect_equal(spec, get_1Dspectrum(1L,start =786243600,end=791506800))
   expect_type(spec,"list")
   expect_equal(names(spec),c("longitude","latitude","frequency1","frequency2","ef","th1m","th2m","sth1m","sth2m","freq","forcings","station"))
-  expect_equal(NROW(spec$forcings),744)
+  expect_equal(NROW(spec$forcings),1488)
   expect_equal(spec$station,"E001500N52000")
 })
 
 
 test_that("downloading 2D spectral data works",{
   skip_if_offline()
-  spec = get_2Dspectrum(1L,start = "1994-01-01 00:00:00 UTC",end="1994-01-01 00:00:00 UTC")
-  expect_equal(spec, get_2Dspectrum(1L,start =757382400,end=757382400))
+  spec = get_2Dspectrum(1L,start = "1994-12-01 00:00:00 UTC",end="1995-01-31 00:00:00 UTC")
+  expect_equal(spec, get_2Dspectrum(1L,start =786243600,end=791506800))
   expect_type(spec,"list")
   expect_equal(names(spec),c("longitude","latitude","frequency1","frequency2","efth","freq","dir","forcings","station"))
-  expect_equal(NROW(spec$forcings),744)
+  expect_equal(NROW(spec$forcings),1488)
   expect_equal(spec$station,"E001500N52000")
 })
