dd6b547f0ee015a6c7f876223ae3f4d63c0122a8
RAILLARD <nicolas.raillard@ifremer.fr>
2024-11-25 09:23:22 +0100

    Last (?) update of files after switching to 'snake_case'

--- R/specta_data_ploting.R
+++ R/specta_data_ploting.R
@@ -11,8 +11,8 @@
 #' @export
 #'
 #' @examplesIf curl::has_internet()
-#' spec <- get_2Dspectrum("SEMREVO", start = "1994-01-01", end = "1994-01-31")
-#' plot_2Dspecta(spec, 1)
+#' spec <- get_2d_spectrum("SEMREVO", start = "1994-01-01", end = "1994-01-31")
+#' plot_2d_specta(spec, 1)
 #' @importFrom ggplot2 ggplot geom_rect scale_x_continuous scale_y_continuous
 #'                     theme_bw coord_polar scale_color_distiller scale_fill_distiller
 #'                     labs expansion
