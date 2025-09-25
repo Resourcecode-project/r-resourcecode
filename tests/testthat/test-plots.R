81997445fca5a9b5ccb12f3ccda9f679e4e8bd69
RAILLARD <nicolas.raillard@ifremer.fr>
2025-09-25 16:56:47 +0200

    Fix tests after updating to ggplot 4.0.0

--- tests/testthat/test-plots.R
+++ tests/testthat/test-plots.R
@@ -1,34 +1,30 @@
 test_that("we can plot the 2D spectrum", {
   skip_if_offline()
-  skip_if(!requireNamespace("resourcecodedata", quietly = TRUE))
   spec <- resourcecodedata::rscd_2d_spectra
   plot_from_index <- plot_2d_specta(spec, 1)
   plot_not_normalized <- plot_2d_specta(spec, 1, normalize = FALSE)
   plot_from_date <- plot_2d_specta(spec, "1994-01-27 13:00:00")
-  expect_warning(print(plot_from_index))
-  expect_warning(print(plot_not_normalized))
-  expect_warning(print(plot_from_date))
-  suppressWarnings({
-    vdiffr::expect_doppelganger(
-      "Specifying index",
-      plot_from_index,
-      variant = Sys.info()[["sysname"]]
-    )
-    vdiffr::expect_doppelganger(
-      "Un-normalized 2D spectra",
-      plot_not_normalized,
-      variant = Sys.info()[["sysname"]]
-    )
-    vdiffr::expect_doppelganger(
-      "Plot by date",
-      plot_from_date,
-      variant = Sys.info()[["sysname"]]
-    )
-  })
+  expect_no_warning(print(plot_from_index))
+  expect_no_warning(print(plot_not_normalized))
+  expect_no_warning(print(plot_from_date))
+  vdiffr::expect_doppelganger(
+    "Specifying index",
+    plot_from_index,
+    variant = Sys.info()[["sysname"]]
+  )
+  vdiffr::expect_doppelganger(
+    "Un-normalized 2D spectra",
+    plot_not_normalized,
+    variant = Sys.info()[["sysname"]]
+  )
+  vdiffr::expect_doppelganger(
+    "Plot by date",
+    plot_from_date,
+    variant = Sys.info()[["sysname"]]
+  )
 })
 
 test_that("Plotting maps works", {
-  skip_if(!requireNamespace("resourcecodedata", quietly = TRUE))
   vdiffr::expect_doppelganger(
     "Mapping water depth",
     rscd_mapplot(resourcecodedata::rscd_field$depth, name = "Depth (m)"),
