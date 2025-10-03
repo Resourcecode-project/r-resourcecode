e67e7b6f1b45a2c6e5d7b673092e4f96e542975e
RAILLARD <nicolas.raillard@ifremer.fr>
2025-10-03 16:47:31 +0200

    New function `plot_1d_specta()` to plot 1D spectral data, with sea-states parameters.

--- R/specta_data_ploting.R
+++ R/specta_data_ploting.R
@@ -13,7 +13,7 @@
 #' @examples
 #' plot_2d_specta(resourcecodedata::rscd_2d_spectra, 1)
 #' @importFrom ggplot2 ggplot geom_rect scale_x_continuous scale_y_continuous
-#'                     theme_bw coord_polar scale_color_distiller scale_fill_distiller
+#'                     theme_linedraw coord_polar scale_color_distiller scale_fill_distiller
 #'                     labs expansion
 plot_2d_specta <- function(
   spec,
@@ -99,6 +99,87 @@ plot_2d_specta <- function(
       subtitle = format(spec$forcings$time[time], format = "%Y-%m-%d %H:%M"),
       caption = "Source: Resourcecode hindcast database\nresourcecode.ifremer.fr"
     ) +
-    theme_bw() +
+    theme_linedraw() +
     theme(legend.position = "bottom")
+}
+
+#' Plot a wave density 1D spectrum at a given time
+#'
+#' @param spec the spectral data, as an output from `get_2Dspectrum`
+#' @param time the time to plot. Either an integer or the date.
+#' @param print_sea_state should the sea_states parameters beeing plot ? Default to TRUE.
+#' @param ... currently unused
+#'
+#' @return a ggplot object
+#' @export
+#'
+#' @examples
+#' plot_1d_specta(resourcecodedata::rscd_1d_spectra, 1)
+#' @importFrom ggplot2 ggplot geom_line scale_x_continuous scale_y_continuous
+#'                     theme_linedraw
+#'                     labs expansion
+plot_1d_specta <- function(
+  spec,
+  time = 1L,
+  print_sea_state = TRUE,
+  ...
+) {
+  if (is.character(time)) {
+    time <- as.POSIXct(time, tz = "UTC")
+  }
+
+  if (inherits(time, "POSIXct")) {
+    time <- which(time == spec$forcings$time)
+  }
+
+  if ("freq" %nin% names(spec)) {
+    warning("Frequency vector not provided: using the default Resourcecode frequency vector.")
+    freq_plot <- rscd_freq
+  } else {
+    freq_plot <- spec$freq
+  }
+
+  df_plot <- tibble::tibble(
+    freq = freq_plot,
+    spectral_density = spec$ef[, time]
+  )
+
+  out_plot <- ggplot(df_plot,
+                     aes(x = .data$freq, y = .data$spectral_density)) +
+    geom_line(col = "#045A8D") + # Colour is the darkest from 'PuBu' for consistency with 2d plot
+    theme_linedraw() +
+    scale_x_continuous(
+                       name = "Frequency (Hz)",
+                       breaks = seq(from = 0, to = 1, by = 0.2),
+                       expand = FALSE) +
+    scale_y_continuous(
+                       name = latex2exp::TeX("Wave spectral density ($m^2s)"),
+                       expand = expansion(c(0, .01), c(0, .05))) +
+    labs(
+         title = paste("Wave energy spectrum at location", spec$station),
+         subtitle = format(spec$forcings$time[time], format = "%Y-%m-%d %H:%M"),
+         caption = "Source: Resourcecode hindcast database\nresourcecode.ifremer.fr")
+  if (print_sea_state) {
+    text <- paste0("**Sea-state parameters:**\n
+Hs=", round(spec$forcings$hs[time], 2), "m<br>
+Tp=", round(1 / spec$forcings$fp[time], 2), "s<br>
+Dir=", round(spec$forcings$dir[time], 2), "°<br>
+Wspd=", round(spec$forcings$wnd[time], 2), "m/s<br>
+Wdir=", round(spec$forcings$wnddir[time], 2), "°<br>")
+
+    out_plot <- out_plot +
+      patchwork::inset_element(
+                               gridtext::textbox_grob(
+                                 text,
+                                 box_gp = grid::gpar(col = "black"),
+                                 r = grid::unit(5, "pt"),
+                                 padding = grid::unit(c(10, 0, 0, 5), "pt"),
+                                 margin = grid::unit(c(0, 0, 0, 0), "pt")
+                               ),
+                               left = 0.7,
+                               bottom = 0.77,
+                               right = .95,
+                               top = .99)
+  }
+  out_plot
 }
