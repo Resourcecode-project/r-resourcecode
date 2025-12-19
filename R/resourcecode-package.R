693de31c00c4a49a98716c63856e225e252bacd1
Nicolas Raillard <nicolas.raillard@ifremer.fr>
2025-12-19 13:43:51 +0100

    Make lint happy

--- R/resourcecode-package.R
+++ R/resourcecode-package.R
@@ -3,7 +3,7 @@
 
 ## usethis namespace: start
 #' @importFrom ggplot2 ggplot aes geom_path coord_sf theme_void geom_polygon scale_fill_distiller
-#'                     scale_color_distiller theme margin element_rect element_blank geom_rect
+#'             scale_color_distiller theme margin element_rect element_blank geom_rect
 #' @importFrom Rcpp sourceCpp evalCpp
 #' @importFrom rlang .data
 #' @useDynLib resourcecode, .registration = TRUE
